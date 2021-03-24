750037007a4e3919e346b358c275088ace64b589
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-03-24 19:18:30 -0400

    options eventlistener added

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const options = document.querySelector("#film-options");
 const filmInfo = document.querySelector("#display-info");
 const title = document.querySelector("#title");
-const year = document.querySelector("release-year");
-const description = document.querySelector("#description");
+const year = document.querySelector("#release-year");
+const descriptions = document.querySelector("#description");
 const form = document.querySelector("form");
 const reviews = document.querySelector("#film-review-ul");
 
 const createOptions = async () => {
     try {
-        const res = await axios.get("https://ghibliapi.herokuapp.com/films")
+        const res = await axios.get(`https://ghibliapi.herokuapp.com/films`)
         res.data.forEach(film => {
             const option = document.createElement("option");
             option.textContent = film.title;
-            option.value = film;
+            option.value = film.id; // index of film in film array!! figure out how to use this
             options.appendChild(option)
         });
+
+
     } catch (err) {
         console.log(err)
     }
   };
   createOptions();
+
+  //should this event listener go inside of create options?
+  options.addEventListener("change", async (e) => {
+      const url = `https://ghibliapi.herokuapp.com/films/${e.target.value}`;
+      const filmsByID = await axios.get(url);
+      title.innerText = filmsByID.data.title
+      year.innerText = filmsByID.data.release_date
+      descriptions.innerText = filmsByID.data.description
+  })
 
   
