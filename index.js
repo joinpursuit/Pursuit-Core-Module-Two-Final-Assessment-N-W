3b5fa9340e4ee279b8d0c3a3170e92a8113a2597
Coreen Cooper <coreencooper@pursuit.org>
2021-03-24 09:02:11 -0400

    display film info

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// - A `section`, empty of text and with the id `display-info`, that will contain the title (`h3`), release year (`p`), and description (`p`) of the selected film.
 // - A `form`, including a "text" `input` and a "submit" `input`, that allows users to submit (not save, just add to the frontend) a review of that film. On submission the input should clear.
 // - A `ul` underneath that `form` that should contain the submitted reviews of each film.
 
-// - Use the `select` box to select a specific film. When they select a film, information about that film (title, release year, description) should populate in the `display-info` section below. When they select a different film, this information should be replaced.
 // - Use the `form`, below the movie information, to submit a review. Each review should be a new `li`, with the selected film's title (in bold) and a review body (not bold).
 
 const movieDirectory = async () => {
@@ -21,11 +19,34 @@ const movieList = (res) => {
     res.data.forEach(movie => {
         const select = document.querySelector("select");
         const option = document.createElement("option");
+        option.value = movie.id;
         option.textContent = movie.title;
         select.appendChild(option);
-
-
     });
 }
+
+// select a specific film and display the title, release year, description. When a different film is selected, the information is replaced.
+const movieSelection = async (e) => {
+    const blank = document.querySelector("#blank");
+    const movieTitle = document.querySelector("#movie-title");
+    const releaseYear = document.querySelector("#release-year");
+    const description = document.querySelector("#description");
+    try {
+        const res = await axios.get(`https://ghibliapi.herokuapp.com/films/${e.target.value}`);
+        movieTitle.textContent = res.data.title
+        releaseYear.textContent = res.data.release_date
+        description.textContent = res.data.description
+        blank.selected = "selected";
+        debugger
+    }
+    catch (err) {
+        console.log("can not compute!")
+    }
+}
+
+const displayInfo = document.querySelector("#display-info");
+displayInfo.addEventListener("click", movieSelection);   
+
+
 
 movieDirectory();
