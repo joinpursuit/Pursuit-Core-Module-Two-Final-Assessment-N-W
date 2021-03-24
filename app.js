2a457f97c7a269c65ada5a3917a0d2fa8a239a5e
Marsha Hall <mphall77@gmail.com>
2021-03-23 20:54:01 -0400

    create movie options

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//curl https://ghibliapi.herokuapp.com
+// https://ghibliapi.herokuapp.com
+
+// selectors
+const select = document.querySelector("select");
+const title = document.querySelector(".title");
+const year = document.querySelector(".release-year");
+const description = document.querySelector(".description");
+
+// functions
+// get movie list
+const getMovies = async (e) => {
+	try {
+		const movieList = await axios.get("https://ghibliapi.herokuapp.com/films");
+		// loop movie list
+		movieList.data.forEach((movie) => {
+			const movieTitle = movie.movieTitle;
+			// create option
+			const option = document.createElement("option");
+			option.textContent = movieTitle;
+			select.appendChild(option);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const getMovieInfo = async (e) => {
+	// movie title -- h3
+	// release year -- p
+	// description
+};
+
+// event listeners
+document.addEventListener("DOMContentLoaded", getMovies);
+select.addEventListener("change", getMovieInfo);
