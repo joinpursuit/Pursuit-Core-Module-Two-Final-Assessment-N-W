8aecb2cb6e2ed6b1e37109ac49519d8a52f050c6
Flower Aviles <itsfloweraviles@gmail.com>
2021-03-24 01:53:41 -0400

    got two functions to work

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-const allCharacters = document.querySelector("#all-characters")
-const main = document.querySelector("main")
-const name = document.querySelector("#character-name")
-const image = document.querySelector("#character-img")
-const status = document.querySelector("#character-status")
-const place = document.querySelector("#character-location")
-const title = document.querySelector("title")
+const main = document.querySelector("main");
+const select = document.querySelector("select");
+const movieInfo = document.querySelector("#display-info");
+const movieTitle = document.querySelector("#movie-title");
+const movieDescription = document.querySelector("#description");
+const movieRelease = document.querySelector("#year-release");
 const form = document.querySelector("#movie-review-form");
-const comments = document.querySelector("#comment")
+const comments = document.querySelector("#comment");
 
-const getAllCharacters = async () => {
+const getAllMovies = async () => {
     try {
         const res = await axios.get('https://ghibliapi.herokuapp.com/films');
 
-        res.data.results.forEach(char => {
-            const li = document.createElement("li")
-            const img = document.createElement("img")
-            const p = document.createElement("p")
-
-            img.src = char.image;
-            img.value = char.id;
-            p.innerText = char.name;
-            debugger
-            li.appendChild(img)
-            li.appendChild(p)
-            allCharacters.appendChild(li)
+        res.data.results.forEach(movie => {
+            const opt = document.createElement('opt');
+            opt.textContent = movie.title;
+            opt.value = movie.id;
+            select.appendChild(opt);
 
 
-        })
+        });
     } catch (err) {
         console.log(err)
-    }
-}
-getAllCharacters()
+    };
+};
+getAllMovies()
 
-allCharacters.addEventListener("click", async (e) => {
+select.addEventListener('change', async (e) => {
 
     main.style.display = "flex"
     const URL = `https://ghibliapi.herokuapp.com/films/${e.target.value}`
@@ -41,9 +33,9 @@ allCharacters.addEventListener("click", async (e) => {
 
     name.innerText = res.data.name;
     image.src = res.data.image;
-    status.innerHTML =  `<b>Status:</b> ${res.data.status}`// innerhtml for extra text
-    place.innerHTML = `<b>Location:</b> ${res.data.location.name}`
-    title.innerText = res.data.name;
+    movieTitle =  res.data.title;
+    movieRelease = res.data.release_date;
+    movieDescription = res.data.description;
 
 
 })
