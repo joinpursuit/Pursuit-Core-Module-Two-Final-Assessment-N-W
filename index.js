1424c4bc4126a15e12b87576db6b7723cbf7c380
Cari <t.carina@rocketmail.com>
2021-03-23 22:23:58 -0400

    films displaying, layout specs passed

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const select = document.querySelector("#titles");
-
+const display = document.querySelector("#display-info");
 
 const getFilms = async () => {
   try {
     const res = await axios.get("https://ghibliapi.herokuapp.com/films");
+
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const p2 = document.createElement("p");
+
+    h3.setAttribute("id", "title")
+    p.setAttribute("id", "year")
+    p2.setAttribute("id", "desc")
+    
+    display.appendChild(h3);
+    display.appendChild(p);
+    display.appendChild(p2);
 
     res.data.forEach((films) => {
       const option = document.createElement("option");
@@ -12,8 +24,19 @@ const getFilms = async () => {
       select.appendChild(option);
     });
   } catch (err) {
-      console.log(err);
+    console.log(err);
   }
 };
 
-getFilms()
+getFilms();
+
+
+
+select.addEventListener("change", async (e) => {
+  const film = await axios.get(
+    `https://ghibliapi.herokuapp.com/films/${e.target.value}`
+  );
+  title.textContent = film.data.title;
+  year.innerHTML = film.data.release_date;
+  desc.textContent = film.data.description;
+});
