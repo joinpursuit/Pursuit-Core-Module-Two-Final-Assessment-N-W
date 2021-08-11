51240b5d346a23f09d9d3bf4d7f383653e93473e
Cari <t.carina@rocketmail.com>
2021-08-11 15:21:02 -0400

    formaat and add catch

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const getFilms = async () => {
     const p = document.createElement("p");
     const p2 = document.createElement("p");
 
-    h3.setAttribute("id", "title")
-    p.setAttribute("id", "year")
-    p2.setAttribute("id", "desc")
-    
+    h3.setAttribute("id", "title");
+    p.setAttribute("id", "year");
+    p2.setAttribute("id", "desc");
+
     display.appendChild(h3);
     display.appendChild(p);
     display.appendChild(p2);
@@ -30,26 +30,28 @@ const getFilms = async () => {
 
 getFilms();
 
-
-
 select.addEventListener("change", async (e) => {
-  const film = await axios.get(
-    `https://ghibliapi.herokuapp.com/films/${e.target.value}`
-  );
-  title.textContent = film.data.title;
-  year.innerHTML = film.data.release_date;
-  desc.textContent = film.data.description;
+  try {
+    const film = await axios.get(
+      `https://ghibliapi.herokuapp.com/films/${e.target.value}`
+    );
+    title.textContent = film.data.title;
+    year.innerHTML = film.data.release_date;
+    desc.textContent = film.data.description;
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-const form = document.querySelector("#rev-form")
-const input = document.querySelector("#review")
-const ul = document.querySelector("#reviews")
+const form = document.querySelector("#rev-form");
+const input = document.querySelector("#review");
+const ul = document.querySelector("#reviews");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const li = document.createElement("li")
-    li.innerHTML = `<b>${title.textContent}:</b> ${input.value}`
-    ul.appendChild(li)
-    
-    input.value = ''
-})
+  e.preventDefault();
+  const li = document.createElement("li");
+  li.innerHTML = `<b>${title.textContent}:</b> ${input.value}`;
+  ul.appendChild(li);
+
+  input.value = "";
+});
